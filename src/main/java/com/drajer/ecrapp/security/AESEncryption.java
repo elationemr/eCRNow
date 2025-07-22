242bc62a69a0d5118c4e40b91c292f3f11fef568
Liam Clarke-Hutchinson <liam@steelsky.co.nz>
2025-07-23 11:24:49 +1200

    This integer overflow thing of CodeQL's is getting silly

--- src/main/java/com/drajer/ecrapp/security/AESEncryption.java
+++ src/main/java/com/drajer/ecrapp/security/AESEncryption.java
@@ -49,15 +49,14 @@ public class AESEncryption {
 
       byte[] cipherText = cipher.doFinal(pText.getBytes());
 
-      long capacity = iv.length + salt.length + cipherText.length;
-
-      if (capacity > Integer.MAX_VALUE - 8) {
-        logger.error("Cipher text capacity exceeds maximum allowed. Capacity: {}", capacity);
-        return null;
-      }
+      // Dear CodeQL - we create iv as a 12 byte array, we create salt as a 16 byte array
+      // It's not an uncontrolled value - now, cipherText _could_ be, so will guard for that
+      // addExact() will throw an exception on any overflow.
+      // Now stop with the vulnerabilities that aren't please
+      int capacity = Math.addExact(iv.length + salt.length, cipherText.length);
 
       byte[] cipherTextWithIvSalt =
-          ByteBuffer.allocate(Math.toIntExact(capacity)).put(iv).put(salt).put(cipherText).array();
+          ByteBuffer.allocate(capacity).put(iv).put(salt).put(cipherText).array();
       return Base64.getEncoder().encodeToString(cipherTextWithIvSalt);
     } catch (Exception ex) {
       logger.error("Error while encrypting:", ex);
